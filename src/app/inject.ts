ba41d1f94a35e32f064c8f095186eaebbe420d52
hlavacka_s <hlavacka_s@yms.sk>
2025-10-21 08:47:38 +0200

    nin

--- src/app/inject.ts
+++ src/app/inject.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Injectable} from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+@Injectable({
+  providedIn: 'root',
+
 })
-export class App {
-  protected readonly title = signal('Learning');
+export class CarService {
+  cars = ['Sunflower GT', 'Flexus Sport', 'Sprout Mach One'];
+
+  getCars(): string[] {
+    return this.cars;
+  }
+
 }
